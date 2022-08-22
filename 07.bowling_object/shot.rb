b6f839c48d83270673f3349c567da6305f03ad34
horiyu <horikoshi@pepabo.com>
2022-08-22 13:54:00 +0900

    add: ショットの初期化処理

--- 07.bowling_object/shot.rb
+++ 07.bowling_object/shot.rb
@@ -4,18 +4,29 @@ class Shot
   attr_accessor(:shots_each_frame)
 
   def initialize(shots)
-    shot_set, @shots_each_frame = []
+    shot_set = []
+    frame_count = 1
+    @shots_each_frame = []
+
     shots.each_with_index do |shot, index|
-      if shot == 'X' && index != 9
+      # ストライクかつ最後のフレーム以外はフレーム＝ストライク
+      if shot == 'X' && frame_count <= 9
         @shots_each_frame << [shot]
-      else
+        frame_count += 1
+        # フレームが最後ではなくショットが最後の時
+      elsif shots.length - 1 == index
         shot_set << shot
-        if index > 9
-          shot_set << shot
-          shot_set = []
-        # 最後のフレームはまとめて値を追加する（2つか3つか分からないため）
-        elsif shots.length - 1 == index
+        @shots_each_frame << shot_set
+        # 最後のショット以降はまとめてフレームに値を追加していく（2つか3つか分からないため）
+      elsif frame_count == 10
+        shot_set << shot
+      else
+        # 最後のフレーム以外かつストライクでない場合は2つショットが揃ったらフレームに追加
+        shot_set << shot
+        if shot_set.length == 2
           @shots_each_frame << shot_set
+          shot_set = []
+          frame_count += 1
         end
       end
     end
