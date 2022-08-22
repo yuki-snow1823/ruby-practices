eafb15ea351ed45f83f19ef170682adf9e00a324
horiyu <horikoshi@pepabo.com>
2022-08-22 12:03:08 +0900

    fix: 変数をまとめる

--- 07.bowling_object/frame.rb
+++ 07.bowling_object/frame.rb
@@ -4,13 +4,7 @@ class Frame
   def initialize(shots_set)
     @shots_set = shots_set
     @frame_score = 0
-  end
-
-  def strike?
-    @shots_set.include?('X')
-  end
-
-  def spare?
-    @shots_set[0] + @shots_set[1] == 10
+    @is_spare = shots_set.include?('X')
+    @is_strike = shots_set[0] + shots_set[1] == 10
   end
 end
