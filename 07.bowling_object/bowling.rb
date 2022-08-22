f4e507a892716cdd754a2eaecb22201f56ef0332
horiyu <horikoshi@pepabo.com>
2022-08-22 11:23:38 +0900

    add ストライクが連続した場合の処置

--- 07.bowling_object/bowling.rb
+++ 07.bowling_object/bowling.rb
@@ -11,7 +11,7 @@ shots_each_frame = shots.shots_each_frame
 is_before_frame_spare = false
 is_before_frame_strike = false
 
-shots_each_frame.each do |shots_pair|
+shots_each_frame.each_with_index do |shots_pair, index|
   frame = Frame.new(shots_pair)
   p frame.shots_pair
   if frame.strike?
@@ -22,6 +22,11 @@ shots_each_frame.each do |shots_pair|
 
   if is_before_frame_strike && frame.strike?
     game.total_score += 10
+    if shots_each_frame[index + 1][0] == 'X'
+      game.total_score += 10
+    else
+      game.total_score += shots_each_frame[index + 1][0].to_i
+    end
   elsif is_before_frame_strike
     game.total_score += (frame.shots_pair[0].to_i + frame.shots_pair[1].to_i)
   end
