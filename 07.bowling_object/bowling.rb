5413dfa08ffcbaaf2d1492ea431f48f095462929
horiyu <horikoshi@pepabo.com>
2022-08-22 14:26:16 +0900

    フレームのスコア計算

--- 07.bowling_object/bowling.rb
+++ 07.bowling_object/bowling.rb
@@ -12,42 +12,16 @@ end
 
 game = Game.new(frames)
 
-p game.frames[0].shots_set
+game.frames.each_with_index do |frame, index|
+  frame.frame_score += if frame.is_strike && frame.frame_no < 10 && frames[index + 1].shots_set == ['X']
+                         20
+                       elsif frame.is_strike && frame.frame_no < 10
+                         frames[index + 1].shots_set.sum(&:to_i) + 10
+                       elsif frame.is_spare && frame.frame_no < 10
+                         10 + frames[index + 1].shots_set[0]
+                       else
+                         frame.shots_set.sum(&:to_i)
+                       end
+end
 
-# shots_each_frame.each_with_index do |shots_set, index|
-#   p frame.shots_set
-
-# 最後X出すとエラーになる その対応が必要
-#   game.total_score += if frame.strike?
-#                         10
-#                       else
-#                         (frame.shots_set[0].to_i + frame.shots_set[1].to_i)
-#                       end
-
-#  p game.shots_each_frame
-
-#   if is_before_frame_strike && frame.strike?
-#     game.total_score += 10
-#     game.total_score += if index < 9 && shots_each_frame[index + 1][0] == 'X'
-#                           10
-#                         elsif index < 9
-#                           shots_each_frame[index + 1][0].to_i
-#                         else
-#                           0
-#                         end
-#   elsif is_before_frame_strike
-#     game.total_score += (frame.shots_set[0].to_i + frame.shots_set[1].to_i)
-#   end
-#   is_before_frame_strike = frame.strike?
-
-#   if is_before_frame_spare && frame.strike?
-#     game.total_score += 10
-#   elsif is_before_frame_spare
-#     game.total_score += frame.shots_set[0].to_i
-#   end
-#   is_before_frame_spare = frame.spare?
-
-#   p game.total_score
-# end
-
-# puts game.total_score
+p game.frames[0].frame_score
