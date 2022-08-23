ccf804b97acfb3c9e1f2563894c3110cfa08fb6f
horiyu <horikoshi@pepabo.com>
2022-08-23 12:28:59 +0900

    fix: メソッドへの切り出し

--- 07.bowling_object/shot.rb
+++ 07.bowling_object/shot.rb
@@ -4,33 +4,39 @@ class Shot
   attr_accessor(:shots_each_frame)
 
   def initialize(shots)
-    shots_set = []
-    frame_no = 1
+    @frame_no = 1
+    @shots_set = []
     @shots_each_frame = []
+    divide_shots_to_frame(shots)
+  end
+
+  def divide_shots_to_frame(shots)
     shots.each_with_index do |shot, index|
-      # ストライクかつ最後のフレーム以外はフレーム＝ストライク
-      if shot == 'X' && frame_no <= 9
+      if shot == 'X' && @frame_no <= 9
         @shots_each_frame << [shot]
-        frame_no += 1
-        # フレームが最後ではなくショットが最後の時
-      elsif frame_no == 10 && shots.length - 1 == index
-        shots_set << shot
-        @shots_each_frame << shots_set
-      elsif frame_no == 10
-        shots_set << shot
+        @frame_no += 1
+      elsif @frame_no == 10 && shots.length - 1 == index
+        add_shots_set_to_frame(shot)
+      elsif @frame_no == 10
+        @shots_set << shot
       elsif shots.length - 1 == index
-        shots_set << shot
-        @shots_each_frame << shots_set
-        # 最後のショット以降はまとめてフレームに値を追加していく（2つか3つか分からないため）
+        add_shots_set_to_frame(shot)
       else
-        # 最後のフレーム以外かつストライクでない場合は2つショットが揃ったらフレームに追加
-        shots_set << shot
-        if shots_set.length == 2
-          @shots_each_frame << shots_set
-          shots_set = []
-          frame_no += 1
-        end
+        @shots_set << shot
+        add_two_shots_set_to_frame if @shots_set.length == 2
       end
     end
   end
+
+  def add_shots_set_to_frame(shot)
+    @shots_set << shot
+    @shots_each_frame << @shots_set
+  end
+
+  def add_two_shots_set_to_frame
+    @shots_each_frame << @shots_set
+    @shots_set = []
+    @frame_no += 1
+  end
+
 end
