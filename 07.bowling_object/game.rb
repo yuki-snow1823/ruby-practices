f219302e0d1c24d587b2342d1a4833dd7ddb3939
yuki-snow1823 <y.horikoshi.pg@gmail.com>
2022-09-02 17:47:42 +0900

    最終ゲームの判定

--- 07.bowling_object/game.rb
+++ 07.bowling_object/game.rb
@@ -4,25 +4,31 @@ class Game
   attr_accessor :all_frames
 
   def initialize
-    @all_frames
+    @all_frames = []
   end
 
   def play
-    # frames = []
-    # frames.each_with_index do |frame, index|
-    #   game.total_score += if frame.frame_no == 9 && frame.is_strike
-    #                         10 + frames[index + 1].shots_set[0] + frames[index + 1].shots_set[1]
-    #                       elsif frame.frame_no != 10 && frame.is_strike && frames[index + 1].is_strike
-    #                         10 + frames[index + 1].shots_set[0] + frames[index + 2].shots_set[0]
-    #                       elsif frame.is_spare
-    #                         10 + frames[index + 1].shots_set[0]
-    #                       elsif frame.is_strike && frame.frame_no != 10
-    #                         10 + frames[index + 1].shots_set[0] + frames[index + 1].shots_set[1]
-    #                       else
-    #                         frame.shots_set.sum
-    #                       end
-    # end
+    10.times do |i|
+      @all_frames[i].score = if i == 9
+                               last_frame_calc
+                             elsif @all_frames[i].spare?
+                               10 + @all_frames[i + 1].first_shot.score
+                             elsif @all_frames[i].strike?
+                               10 + @all_frames[i + 1].first_shot.score + @all_frames[i + 1].second_shot.score
+                             else
+                               @all_frames[i].first_shot.score + @all_frames[i].second_shot.score
+                             end
+    end
+    p @all_frames.sum(&:score)
+  end
 
-    # p game.total_score
+  private
+
+  def last_frame_calc
+    if @all_frames.last.strike?
+      10 + @all_frames.last.second_shot.score + @all_frames.last.third_shot.score
+    elsif @all_frames.last.spare?
+      10 + @all_frames.last.third_shot.score
+    end
   end
 end
