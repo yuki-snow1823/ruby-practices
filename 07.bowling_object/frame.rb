30c37b57677a2394841af693dbc750da59cc25c5
yuki-snow1823 <y.horikoshi.pg@gmail.com>
2022-09-05 17:49:14 +0900

    不要なローカル変数を削除するためArray.newを使用（mapはrubocopによって変更）

--- 07.bowling_object/frame.rb
+++ 07.bowling_object/frame.rb
@@ -12,19 +12,16 @@ class Frame
   end
 
   def self.divide_all_shots_to_frames(input_shots, game)
-    frames = []
     shots = input_shots.split(',')
-    10.times do |i|
-      frames << if i == 9
-                  Frame.new(*shots)
-                elsif shots.first == 'X'
-                  Frame.new(shots.shift)
-                else
-                  Frame.new(shots.shift, shots.shift)
-                end
+    game.frames = Array.new(10) do |i|
+      if i == 9
+        Frame.new(*shots)
+      elsif shots.first == 'X'
+        Frame.new(shots.shift)
+      else
+        Frame.new(shots.shift, shots.shift)
+      end
     end
-    game.frames = frames
-    p input_shots
   end
 
   def strike?
