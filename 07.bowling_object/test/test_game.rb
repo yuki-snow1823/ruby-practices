c98e897c4d6828d5fb3155e5279bdd95dc899dfe
yuki-snow1823 <y.horikoshi.pg@gmail.com>
2022-09-05 18:00:41 +0900

    自前のクラスをrequire_relativeに参照方法変更

--- 07.bowling_object/test/test_game.rb
+++ 07.bowling_object/test/test_game.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
-
+require_relative '../bowling'
 require 'minitest/autorun'
 require 'minitest/unit'
 
 MiniTest::Unit.autorun
 
 class TestBowling < MiniTest::Unit::TestCase
-  require '../bowling'
+
   def test_score
     process_args('X,X,X,X,X,X,X,X,X,X,X,X')
   end
