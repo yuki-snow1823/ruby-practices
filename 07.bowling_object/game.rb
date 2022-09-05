8686266b6de39f9c80a7daf14ff6ca5bde51bcd4
yuki-snow1823 <y.horikoshi.pg@gmail.com>
2022-09-05 16:59:18 +0900

    all_frameとframesの違いがなかったため、よりシンプルにした

--- 07.bowling_object/game.rb
+++ 07.bowling_object/game.rb
@@ -1,55 +1,55 @@
 # frozen_string_literal: true
 
 class Game
-  attr_accessor :all_frames
+  attr_accessor :frames
 
   def initialize(input_shots)
-    @all_frames = []
+    @frames = []
     10.times do |i|
-      @all_frames << if i == 9 && (input_shots.first == 'X' || input_shots.first.to_i + input_shots[1].to_i == 10)
-                       Frame.new(input_shots.shift, input_shots.shift, input_shots.shift)
-                     elsif input_shots.first == 'X'
-                       Frame.new(input_shots.shift)
-                     else
-                       Frame.new(input_shots.shift, input_shots.shift)
-                     end
+      @frames << if i == 9 && (input_shots.first == 'X' || input_shots.first.to_i + input_shots[1].to_i == 10)
+                   Frame.new(input_shots.shift, input_shots.shift, input_shots.shift)
+                 elsif input_shots.first == 'X'
+                   Frame.new(input_shots.shift)
+                 else
+                   Frame.new(input_shots.shift, input_shots.shift)
+                 end
     end
   end
 
   def play
     10.times do |i|
-      @all_frames[i].score = if i == 9
-                               last_frame_calc
-                             elsif @all_frames[i].strike?
-                               strike_calc(i)
-                             elsif @all_frames[i].spare?
-                               10 + @all_frames[i + 1].first_shot.score
-                             else
-                               @all_frames[i].first_shot.score + @all_frames[i].second_shot.score
-                             end
+      @frames[i].score = if i == 9
+                           last_frame_calc
+                         elsif @frames[i].strike?
+                           strike_calc(i)
+                         elsif @frames[i].spare?
+                           10 + @frames[i + 1].first_shot.score
+                         else
+                           @frames[i].first_shot.score + @frames[i].second_shot.score
+                         end
     end
-    p @all_frames.sum(&:score)
+    p @frames.sum(&:score)
   end
 
   private
 
   def last_frame_calc
-    if @all_frames.last.strike?
-      10 + @all_frames.last.second_shot.score + @all_frames.last.third_shot.score
-    elsif @all_frames.last.spare?
-      10 + @all_frames.last.third_shot.score
+    if @frames.last.strike?
+      10 + @frames.last.second_shot.score + @frames.last.third_shot.score
+    elsif @frames.last.spare?
+      10 + @frames.last.third_shot.score
     else
-      @all_frames.last.first_shot.score + @all_frames.last.second_shot.score
+      @frames.last.first_shot.score + @frames.last.second_shot.score
     end
   end
 
   def strike_calc(frame_count)
-    if frame_count == 8 && @all_frames[frame_count + 1].strike?
-      10 + @all_frames[frame_count + 1].first_shot.score + @all_frames[frame_count + 1].second_shot.score
-    elsif @all_frames[frame_count + 1].strike?
-      20 + @all_frames[frame_count + 2].first_shot.score
+    if frame_count == 8 && @frames[frame_count + 1].strike?
+      10 + @frames[frame_count + 1].first_shot.score + @frames[frame_count + 1].second_shot.score
+    elsif @frames[frame_count + 1].strike?
+      20 + @frames[frame_count + 2].first_shot.score
     else
-      10 + @all_frames[frame_count + 1].first_shot.score + @all_frames[frame_count + 1].second_shot.score
+      10 + @frames[frame_count + 1].first_shot.score + @frames[frame_count + 1].second_shot.score
     end
   end
 end
