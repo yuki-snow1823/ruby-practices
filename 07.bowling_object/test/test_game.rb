0a975697d532243b143b47f9466acf9d8c71410f
yuki-snow1823 <y.horikoshi.pg@gmail.com>
2022-09-08 14:54:00 +0900

    タイポの修正

--- 07.bowling_object/test/test_game.rb
+++ 07.bowling_object/test/test_game.rb
@@ -24,13 +24,13 @@ class TestBowling < MiniTest::Unit::TestCase
   def test_last_frame_and_the_just_before_frame_are_strike
     input_shots = '6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,X,0,0'
     game = Game.new(input_shots)
-    assert_equal(144, game.play)
+    assert_equal(134, game.play)
   end
 
   def test_last_frame_has_score_and_the_just_before_frame_are_strike
     input_shots = '6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,X,1,8'
     game = Game.new(input_shots)
-    assert_equal(134, game.play)
+    assert_equal(144, game.play)
   end
 
   def test_all_strike
