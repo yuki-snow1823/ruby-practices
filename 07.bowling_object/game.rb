7da32f656a91fb83c3fb1f9982c805e1280eab26
yuki-snow1823 <y.horikoshi.pg@gmail.com>
2022-09-08 16:27:29 +0900

    to_iが不要だったため削除

--- 07.bowling_object/game.rb
+++ 07.bowling_object/game.rb
@@ -11,7 +11,7 @@ class Game
     total_score = 0
     10.times do |i|
       total_score += if i == 9
-                       @frames.last.first_shot.score.to_i + @frames.last.second_shot.score.to_i + @frames.last.third_shot.score.to_i
+                       @frames.last.first_shot.score + @frames.last.second_shot.score + @frames.last.third_shot.score.to_i
                      elsif @frames[i].strike?
                        strike_calc_except_last_frame(i)
                      elsif @frames[i].spare?
