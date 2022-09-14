7bec5c81e89e3bd55492271229e388c5450d3806
yuki-snow1823 <y.horikoshi.pg@gmail.com>
2022-09-14 10:37:15 +0900

    eachのループをマップに

--- 08.ls_object/ls.rb
+++ 08.ls_object/ls.rb
@@ -24,9 +24,7 @@ options = ls_app.options
 
 case options
 when ['a']
-  ls_app.display_all.each do |l|
-    puts l.join('')
-  end
+  ls_app.display_all.each {|l| puts l.join('') }
 when ['r']
   ls_app.display_reverse
 when ['l']
