6643ff3a02df00f5d0c14030e24d8c9ae566f8fe
yuki-snow1823 <y.horikoshi.pg@gmail.com>
2022-10-26 06:55:35 +0900

    rjustの戻り値は文字列なのでそのまま活用

--- 08.ls_object/ls_app.rb
+++ 08.ls_object/ls_app.rb
@@ -38,10 +38,10 @@ class LsApp
     @ls_files.each do |ls_file|
       output = []
       output << "#{ls_file.type}#{ls_file.permission} "
-      output << ls_file.nlink.to_s.rjust(max_nlink_count).to_s
-      output << ls_file.owner.to_s.rjust(max_owner_count).to_s
-      output << ls_file.group.to_s.rjust(max_group_count).to_s
-      output << " #{ls_file.size.to_s.rjust(max_ls_file_size_count).to_s}"
+      output << ls_file.nlink.to_s.rjust(max_nlink_count)
+      output << ls_file.owner.to_s.rjust(max_owner_count)
+      output << ls_file.group.to_s.rjust(max_group_count)
+      output << " #{ls_file.size.to_s.rjust(max_ls_file_size_count)}"
       output << ls_file.modified_time.to_s
       output << ls_file.name.to_s
       puts output.join(' ')
