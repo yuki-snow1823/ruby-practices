3c4ca6cdc5399467ca65a18f502cecba39fa9549
yuki-snow1823 <y.horikoshi.pg@gmail.com>
2022-10-25 19:28:34 +0900

    マッピングをlsfile定義の中に移動してわかりやすくした

--- 08.ls_object/ls_file.rb
+++ 08.ls_object/ls_file.rb
@@ -1,12 +1,33 @@
 # frozen_string_literal: true
 
-require_relative './file_detail_map'
 require 'etc'
 
 class LsFile
   attr_reader :name, :file_stat
 
   DIRECTORY_NUMBER = '40'
+
+  PERMISSION_MAP = {
+    '0' => '---',
+    '1' => '--x',
+    '2' => '-w-',
+    '3' => '-wx',
+    '4' => 'r--',
+    '5' => 'r-x',
+    '6' => 'rw-',
+    '7' => 'rwx'
+  }.freeze
+  
+  TYPE_MAP = {
+    '01' => 'p',
+    '02' => 'c',
+    '40' => 'd',
+    '06' => 'b',
+    '10' => '-',
+    '12' => 'l',
+    '14' => 's'
+  }.freeze
+  
 
   def initialize(file_name)
     @name = file_name
